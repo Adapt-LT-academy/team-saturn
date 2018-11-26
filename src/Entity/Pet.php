d4ab996c4799b4f660cc9a755fea907b0c56eaa3
Chateux1 <m.satas@gmail.com>
2018-11-26 21:52:01 +0200

    edit pet entity

--- src/Entity/Pet.php
+++ src/Entity/Pet.php
@@ -10,47 +10,83 @@ use Doctrine\ORM\Mapping as ORM;
 class Pet
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
+     * @var int
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      * @ORM\Column(type="integer")
+     *
      */
     private $id;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $species;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=20)
      */
     private $size;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=20)
      */
     private $sex;
 
     /**
+     * @var string
+     *
      * @ORM\Column(type="string", length=255)
      */
     private $image;
 
     /**
+     * @var int
+     *
      * @ORM\Column(type="integer")
      */
     private $price;
 
+    /**
+     * @return int
+     */
     public function getId(): ?int
     {
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     *
+     * @return $this
+     */
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getSpecies(): ?string
     {
         return $this->species;
     }
 
+    /**
+     * @param string $species
+     *
+     * @return $this
+     */
     public function setSpecies(string $species): self
     {
         $this->species = $species;
@@ -58,11 +94,19 @@ class Pet
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getSize(): ?string
     {
         return $this->size;
     }
 
+    /**
+     * @param string $size
+     *
+     * @return $this
+     */
     public function setSize(string $size): self
     {
         $this->size = $size;
@@ -70,11 +114,19 @@ class Pet
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getSex(): ?string
     {
         return $this->sex;
     }
 
+    /**
+     * @param string $sex
+     *
+     * @return $this
+     */
     public function setSex(string $sex): self
     {
         $this->sex = $sex;
@@ -82,11 +134,19 @@ class Pet
         return $this;
     }
 
+    /**
+     * @return string
+     */
     public function getImage(): ?string
     {
         return $this->image;
     }
 
+    /**
+     * @param string $image
+     *
+     * @return $this
+     */
     public function setImage(string $image): self
     {
         $this->image = $image;
@@ -94,11 +154,19 @@ class Pet
         return $this;
     }
 
+    /**
+     * @return int
+     */
     public function getPrice(): ?int
     {
         return $this->price;
     }
 
+    /**
+     * @param int $price
+     *
+     * @return $this
+     */
     public function setPrice(int $price): self
     {
         $this->price = $price;
