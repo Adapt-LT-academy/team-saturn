7614aa6a36a95327c98920f0d7ce557f429e8b53
mantaser <mantaseringis@gmail.com>
2018-12-05 15:29:23 +0200

    add runBrowse method

--- src/Service/WelcomeConversation.php
+++ src/Service/WelcomeConversation.php
@@ -15,52 +15,96 @@ use App\Traits\ContainerAwareConversationTrait;
  * Class TestConversation
  * @property  startConversation
  */
-class WelcomeConversation extends Conversation {
-    
+class WelcomeConversation extends Conversation
+{
+
     use ContainerAwareConversationTrait;
 
+    protected $species;
+    protected $kind;
+    protected $size;
+    protected $sex;
+    protected $price;
+    protected $name;
+    protected $surname;
+    protected $street;
+    protected $city;
+    protected $postal;
 
-    public function run() {
-     //   $this->ask('Select one of above');
-          $question = Question::create('Select one of above')
-              ->addButtons([
-                  Button::create('Browse')->value('startBrowse'),
-                  Button::create('Random')->value('startRandom'),
-                  Button::create('Quit')->value('startQuit'),
-              ]);
+    public function run()
+    {
+        //   $this->ask('Select one of above');
+        $question = Question::create('Select one of above')
+            ->addButtons([
+                Button::create('Browse')->value('startBrowse'),
+                Button::create('Random')->value('startRandom'),
+                Button::create('Quit')->value('startQuit'),
+            ]);
 
-          $this->ask($question, function (Answer $answer) {
-              // Detect if button was clicked:
-              if ($answer->isInteractiveMessageReply()) {
-                  $selectedValue = $answer->getValue(); // will be either 'startBrowse', 'startRandom' or 'startQuit'
-//                $selectedText = $answer->getText(); // etc..
-              }
+        $this->ask($question, function (Answer $answer) {
+            // Detect if button was clicked:
+            if ($answer->isInteractiveMessageReply()) {
+                $answer->getValue();
+            }
 
-              switch ($answer){
-                  case 'startBrowse':
-                      //$this->startConversation(new Browse);
-                      $this->runBrowse();
-                      break;
+            switch ($answer) {
+                case 'startBrowse':
+                    $this->runBrowse();
+                    break;
 
-                  case 'startRandom':
-                      $this->runBrowse();
-                      break;
+                case 'startRandom':
+                    $this->runRandom();
+                    break;
 
-                  case 'startQuit':
-                      $this->runBrowse();
-                      break;
-              }
-              // printout selected
- //             $this->say("You pressed $selectedValue");
-
-          });
+                case 'startQuit':
+                    $this->runQuit();
+                    break;
+            }
+        });
     }
 
 
-    public function runBrowse() {
-        $this->say('runBrowse executed');
-     //   $this->startConversation (new Browse());
+    public function runBrowse()
+    {
+        // $this->say('runBrowse executed');
+        $question = Question::create('What pet would you prefer?')
+            ->addButtons([
+                Button::create('Dog')->value('dog'),
+                Button::create('Cat')->value('cat'),
+            ]);
+
+        $this->ask($question, function (Answer $answer) {
+            if ($answer->isInteractiveMessageReply()) {
+                $answer->getValue();
+            }
+
+            if ($answer == 'dog') {
+                $this->species = $answer;
+                $this->selectDogKind();
+            } else {
+                $this->species = $answer;
+                $this->selectCatKind();
+            }
+        });
     }
 
+    public function runRandom()
+    {
+        $this->say('runRandom executed');
+    }
 
+    public function runQuit()
+    {
+        $this->say('Service stopped');
+    }
+
+    public function selectDogKind()
+    {
+        $this->say('selectDogKind executed..');
+    }
+
+    public function selectCatKind()
+    {
+        $this->say('selectCatKind executed..');
+    }
 }
